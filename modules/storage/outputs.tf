ea215f7870c9b8e8e6b744ea65a6f841d87f30e9
Dinithi Yapa <136271101+Dinithiyapa@users.noreply.github.com>
2025-03-16 16:00:34 +0530

    Create outputs.tf

---
+++ modules/storage/outputs.tf
@@ -0,0 +1,18 @@
+# Storage Account Name
+output "storage_account_name" {
+  description = "The name of the Azure Storage Account"
+  value       = azurerm_storage_account.storage.name
+}
+
+# Storage Account Primary Key (Sensitive)
+output "storage_account_primary_key" {
+  description = "The primary access key for the Storage Account"
+  value       = azurerm_storage_account.storage.primary_access_key
+  sensitive   = true
+}
+
+# Storage Container URL
+output "storage_container_url" {
+  description = "The URL of the Blob Storage Container"
+  value       = azurerm_storage_container.container.id
+}
