bef9acaec557d76206f0dbce348ebdb123ec26c6
Dinithi Yapa <136271101+Dinithiyapa@users.noreply.github.com>
2025-03-16 18:46:20 +0530

    Delete modules/auth directory

--- modules/auth/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "clerk_auth_backend" {
-  description = "Backend App Service integrated with Clerk authentication"
-  value       = azurerm_linux_web_app.backend_auth.name
-}
-
-output "clerk_publishable_key" {
-  description = "Clerk publishable key used for frontend integration"
-  value       = var.clerk_publishable_key
-}
