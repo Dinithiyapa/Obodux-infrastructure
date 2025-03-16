1953599c15bb8b2a490820ed073fce0a794cd6d7
Dinithi Yapa <136271101+Dinithiyapa@users.noreply.github.com>
2025-03-16 19:12:06 +0530

    Update outputs.tf

--- modules/realtime/outputs.tf
+++ modules/realtime/outputs.tf
@@ -1,21 +1,26 @@
 output "signalr_id" {
-  description = "The ID of the Azure SignalR Service"
+  description = "The ID of the created SignalR service"
   value       = azurerm_signalr_service.signalr.id
 }
 
-output "signalr_hostname" {
-  description = "The primary hostname of the SignalR service"
-  value       = azurerm_signalr_service.signalr.hostname
+output "signalr_name" {
+  description = "The name of the created SignalR service"
+  value       = azurerm_signalr_service.signalr.name
 }
 
 output "signalr_primary_connection_string" {
-  description = "The primary connection string for SignalR"
+  description = "Primary connection string for SignalR service"
   value       = azurerm_signalr_service.signalr.primary_connection_string
   sensitive   = true
 }
 
-output "signalr_secondary_connection_string" {
-  description = "The secondary connection string for SignalR"
-  value       = azurerm_signalr_service.signalr.secondary_connection_string
+output "signalr_primary_access_key" {
+  description = "Primary access key for SignalR service"
+  value       = azurerm_signalr_service.signalr.primary_access_key
   sensitive   = true
+}
+
+output "signalr_hostname" {
+  description = "The hostname of the SignalR service"
+  value       = azurerm_signalr_service.signalr.hostname
 }
