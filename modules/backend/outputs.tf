c82e90d6de8f833b4fca7771b4041b8d59a9eb73
Dinithi Yapa <136271101+Dinithiyapa@users.noreply.github.com>
2025-03-16 15:11:40 +0530

    Create outputs.tf

---
+++ modules/backend/outputs.tf
@@ -0,0 +1,19 @@
+output "app_service_plan_id" {
+  description = "The ID of the Backend App Service Plan."
+  value       = azurerm_app_service_plan.backend.id
+}
+
+output "web_app_hostname" {
+  description = "The hostname of the Backend Web App."
+  value       = azurerm_linux_web_app.backend.default_hostname
+}
+
+output "web_app_identity_principal_id" {
+  description = "The Principal ID of the Backend Web App's System Assigned Managed Identity."
+  value       = azurerm_linux_web_app.backend.identity[0].principal_id
+}
+
+output "backend_url" {
+  description = "The url of the backend"
+  value = "https://${azurerm_linux_web_app.backend.default_hostname}"
+}
