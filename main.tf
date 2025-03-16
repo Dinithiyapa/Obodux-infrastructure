318a62c290536837512f58ce4a174c67e97f3e0a
Dinithi Yapa <136271101+Dinithiyapa@users.noreply.github.com>
2025-03-16 19:51:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,8 +27,8 @@ module "networking" {
 }
 
 # File Storage Module
-module "file_storage" {
-  source                  = "./modules/file_storage"
+module "storage" {
+  source                  = "./modules/storage"
   resource_group_name     = module.resource_group.name
   location                = var.location
   storage_account_name    = var.storage_account_name
