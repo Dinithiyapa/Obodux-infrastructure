c0f03012229e3ffafcd25d5717c2b1087b8daebf
Dinithi Yapa <136271101+Dinithiyapa@users.noreply.github.com>
2025-03-16 15:11:21 +0530

    Create variable.tf

---
+++ modules/backend/variable.tf
@@ -0,0 +1,61 @@
+variable "location" {
+  description = "The Azure region where resources will be deployed."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "app_subnet_id" {
+  description = "The ID of the subnet where the backend will be deployed."
+  type        = string
+}
+
+
+variable "backend_app_name" {
+  description = "Name of the backend application"
+  type        = string
+}
+
+variable "backend_docker_image_tag" {
+  description = "Docker image tag for backend"
+  type        = string
+}
+
+variable "database_connection_string" {
+  description = "PostgreSQL connection string"
+  type        = string
+}
+
+variable "clerk_secret_key" {
+  description = "Clerk secret key for authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "clerk_publishable_key" {
+  description = "Clerk publishable key"
+  type        = string
+}
+
+variable "redis_connection_string" {
+  description = "Redis connection string for BullMQ"
+  type        = string
+}
+
+variable "notifications_service_connection_string" {
+  description = "Azure Communication Services connection string"
+  type        = string
+}
+variable "clerk_secret_key" {
+  description = "Clerk secret key for authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "clerk_publishable_key" {
+  description = "Clerk publishable key for authentication"
+  type        = string
+}
