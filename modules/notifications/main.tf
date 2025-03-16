f452a08c96800ed463a4aa036094dfdaeef26a34
Dinithi Yapa <136271101+Dinithiyapa@users.noreply.github.com>
2025-03-16 15:09:35 +0530

    Create main.tf

---
+++ modules/notifications/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_communication_service" "notifications" {
+  name                = var.notifications_service_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  data_location       = "United States"  # Choose appropriate data residency
+
+  tags = {
+    environment = var.environment
+  }
+}
